66c35bfba7cfda83b1c506ba13b65024bc117d64
William Andres Santiesteban <wsantiesteban@easysol.net>
2019-12-12 22:48:16 -0500

    modificaciones echos por hernan

--- JavaFXJuegoHangman/src/javafxjuegohangman/FXMLDocumentController.java
+++ JavaFXJuegoHangman/src/javafxjuegohangman/FXMLDocumentController.java
@@ -26,6 +26,20 @@ public class FXMLDocumentController implements Initializable {
     private AnchorPane Aviso2;
     @FXML
     private AnchorPane Instrucciones;
+    @FXML
+    private AnchorPane MultijugadorMenu;
+    @FXML
+    private AnchorPane DeportesMultipayer1;
+    @FXML
+    private AnchorPane AtencionMultijugador;
+    @FXML
+    private AnchorPane AvisoDeportesMenu;
+    @FXML
+    private AnchorPane IdiomasMultiPlayer1;
+    @FXML
+    private AnchorPane AtencionMultiIdioma;
+    @FXML 
+    private AnchorPane AvisoIdiomaMenu;
     
     //Boton de salir
     
@@ -53,12 +67,54 @@ public class FXMLDocumentController implements Initializable {
         Instrucciones.setVisible(true);
     }
     
+    //Boton para ir al modo de juego de dos jugadores 
+    
+    @FXML
+    private void ModoDeJuegoMultiplayer (ActionEvent e)
+    {
+        Menu.setVisible(false);
+        MultijugadorMenu.setVisible(true);
+    }
+    
+    //Boton para ir al modo de juego deportes de Dos jugadores 
+    
+    @FXML
+    private void ModoDeJuegoDeportesMultiplayer (ActionEvent e)
+    {
+        MultijugadorMenu.setVisible(false);
+        DeportesMultipayer1.setVisible(true);
+        AtencionMultijugador.setVisible(true);
+    }
+    
+    //Boton para ir al modo de juego idiomas de dos jugadores
+    @FXML
+    private void ModoDeJuegoIdiomaMultiplayer (ActionEvent e)
+    {
+        MultijugadorMenu.setVisible(false);
+        IdiomasMultiPlayer1.setVisible(true);
+        AtencionMultiIdioma.setVisible(true);
+    }
+    
+    @FXML
+    private void cerrarAvisoMultplayer (ActionEvent e)
+    {
+        AtencionMultijugador.setVisible(false);
+        AtencionMultiIdioma.setVisible(false);
+    }
+    
     //Boton volver al Menu
     
     @FXML
     private void VolverMenu (ActionEvent e)
     {
         ModoUnJugador.setVisible(false);
+        Menu.setVisible(true);
+    }
+    
+    @FXML
+    private void VolverMenu2 (ActionEvent e)
+    {
+        MultijugadorMenu.setVisible(false);
         Menu.setVisible(true);
     }
     
@@ -104,10 +160,50 @@ public class FXMLDocumentController implements Initializable {
     }
     
     @FXML
+    private void IralIdiomasMenu(ActionEvent e)
+    {
+        AvisoIdiomaMenu.setVisible(true);
+    }
+    
+    @FXML
+    private void SiSigue3(ActionEvent e)
+    {
+        Menu.setVisible(true);
+        AvisoIdiomaMenu.setVisible(false);
+        IdiomasMultiPlayer1.setVisible(false);
+    }
+    
+    @FXML
+    private void NoSigue3(ActionEvent e)
+    {
+        AvisoIdiomaMenu.setVisible(false);
+    }
+    
+    @FXML
     private void irMenuInstrucciones(ActionEvent e)
     {
         Instrucciones.setVisible(false);
         Menu.setVisible(true);
+    }
+    
+    @FXML
+    private void VolverMenuDeportesMulti (ActionEvent e)
+    {
+        AvisoDeportesMenu.setVisible(true);
+    }
+    
+    @FXML
+    private void SiDeportesMenu(ActionEvent e)
+    {
+        AvisoDeportesMenu.setVisible(false);
+        DeportesMultipayer1.setVisible(false);
+        Menu.setVisible(true);
+    }
+    
+    @FXML
+    private void NoDeportesMenu(ActionEvent e)
+    {
+        AvisoDeportesMenu.setVisible(false);
     }
             
     
